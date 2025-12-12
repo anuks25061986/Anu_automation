ebba4b72e3e64ad1448f4925a55223dafeaa7b2b
anuks25061986 <anuradhakumari.singh2009@gmail.com>
2025-12-12 14:35:02 +0530

    RMS-123 Change in Hello world program.

--- src/test/java/Steps/HelloWorld.java
+++ src/test/java/Steps/HelloWorld.java
@@ -3,6 +3,7 @@ package Steps;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is my first github commit integrating with jenkins");
+        System.out.println("This is commit from feature into master");
     }
 }
 
