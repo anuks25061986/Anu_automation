f0f72dd16a3aab61bdb9b3295b2898bef6eafb01
anuks25061986 <anuradhakmr278@gmail.com>
2026-01-04 01:15:06 +0530

    Demo for git rebase

--- src/test/java/Steps/HelloWorld.java
+++ src/test/java/Steps/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is my first github commit integrating with jenkins");
         System.out.println("This is commit from feature into master");
+        System.out.println("This demo is for git rebase from intellij");
     }
 }
+
 
