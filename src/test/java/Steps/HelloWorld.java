2b949b5d7c83138065e40809210dab504e76d0d8
anuks25061986 <anuradhakmr278@gmail.com>
2026-01-09 22:47:04 +0530

    Correction in jenkins

--- src/test/java/Steps/HelloWorld.java
+++ src/test/java/Steps/HelloWorld.java
@@ -2,11 +2,12 @@ package Steps;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("This is my first github commit integrating with jenkins");
+        System.out.println("This is my first github commit integrating with jenkinsssss");
         System.out.println("This is commit from feature into master");
         System.out.println("This demo is for git rebase from intellij");
     }
 }
+
 
 
 
